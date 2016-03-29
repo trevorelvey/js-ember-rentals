a14b7e7b2d7148b0d83657386cc238922dd75546
trevorelvey <trevor.elvey@gmail.com>
2016-03-29 11:53:11 -0700

    complete super rentals project

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
+  updateRentalForm: false,
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
     },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+    update(rental, params) {
+      this.sendAction('update', rental, params);
     },
     delete(rental) {
       if(confirm('Are you sure you want to delete this rental?')) {
